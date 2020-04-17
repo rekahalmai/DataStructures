22b0264d02c15153ffa70affe9e41030ce1e7eaa
rekahalmai <rekahalmai@gmail.com>
2020-04-17 18:09:11 +0200

    almost done linkedlist class

--- LinkedList.py
+++ LinkedList.py
@@ -34,6 +34,9 @@ class Node():
     def is_start(self):
         return self.previous == None
 
+    def __call__(self, *args, **kwargs):
+        return
+
 
 class LinkedList():
 
@@ -59,19 +62,18 @@ class LinkedList():
         _node.previous, _node.next = None, None
         self.head = _node
         self._len += 1
+        print("added first node")
 
 
     def insert_node_at_end(self, node):
         _node = node
-
-        current_node = self.head()
+        current_node = self.head
         while current_node.next is not None:
             current_node = current_node.next
 
-        current_node.next = _node
-
         _node.previous = current_node
         _node.next = None
+        current_node.next = _node
 
         self._len += 1
 
@@ -93,12 +95,14 @@ class LinkedList():
             while current_node.next is not None:
                 current_node = current_node.next
                 previous_node = current_node
+
             previous_node.next = None
+            current_node.data, current_node.previous, current_node.next = None, None, None
+            #current_node.previous, current_node.data = None, None
             self._len -= 1
 
 
     def create_node_value_list(self):
-
         current_node = self.head
         node_value_list = []
         while current_node is not None:
@@ -107,25 +111,70 @@ class LinkedList():
         return node_value_list
 
 
-    def print_values_in_list(self):
+    def print_values(self):
         node_value_list = self.create_node_value_list()
         print(node_value_list)
-        return
 
     def search_value_in_list(self, value):
+        """"""
         current_node = self.head
         results = []
         while current_node is not None:
-            if current_node.value == value:
+            if current_node.data == value:
                 print(f"Found the value {value} at node {current_node}")
                 results.append(current_node)
-
+                current_node = current_node.next
+            else:
+                current_node = current_node.next
+        print(results)
         return results
 
 
     def delete_value(self, value):
+        """ Delete the nodes that have the same value as value """
         current_node, previous_node = self.head, self.head
 
-        while current_node is not None:
-            if current_node.value == value:
+        while current_node.next is not None:
+            if current_node.data == value:
+                print(f"Found the value {value} at node {current_node}")
                 previous_node.next = current_node.next
+                current_node =current_node.next
+                self._len -= 1
+            else:
+                previous_node = current_node
+                current_node = current_node.next
+
+
+
+
+def main():
+    node1 = Node("H")
+    node2 = Node("I")
+    node3 = Node("T")
+    node4 = Node("H")
+    node5 = Node("E")
+    node6 = Node("R")
+    node7 = Node("E")
+
+    dl_list = LinkedList()
+    dl_list.add_node(node1)
+    dl_list.add_node(node2)
+    dl_list.add_node(node3)
+    dl_list.add_node(node4)
+    dl_list.add_node(node5)
+    dl_list.add_node(node6)
+    dl_list.add_node(node7)
+    dl_list.print_values()
+
+    dl_list.delete_first_node()
+    dl_list.print_values()
+
+    dl_list.delete_last_node()
+    dl_list.print_values()
+
+    dl_list.delete_value('E')
+    dl_list.print_values()
+
+
+if __name__ == "__main__":
+    main()
